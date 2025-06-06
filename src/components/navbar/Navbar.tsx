4dce827ce0bfe5c035c0faf277a4d2805df9c67d
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-06-06 20:42:13 -0300

    barebones

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,9 @@
+import './navbar.css';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <span>Transaction Builder</span>
+    </div>
+  );
 }
--- src/views/txview/TxView.tsx
+++ src/views/txview/TxView.tsx
@@ -1,3 +1,3 @@
 export default function TxView() {
-  return <>TxView</>;
+  return <div></div>;
 }
