6d7d13bde8eaa92d488bc9c52a632aff3a10c9ad
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-06-07 16:21:17 -0300

    tests for encoding varint

--- src/util/__tests__/helper.test.ts
+++ src/util/__tests__/helper.test.ts
@@ -1,4 +1,4 @@
-import { littleEndianToInteger, integerToLittleEndian } from '../helper';
+import { littleEndianToInteger, integerToLittleEndian, readVarInt } from '../helper';
 
 describe('littleEndianToInteger', () => {
   test('correct conversion', () => {
@@ -27,10 +27,48 @@ describe('integerToLittleEndian', () => {
 
   test('correct conversion 2', () => {
     const n = 10011545;
-    
+
     const want = new Uint8Array([0x99, 0xc3, 0x98, 0x00, 0x00, 0x00, 0x00, 0x00]);
     const got = integerToLittleEndian(BigInt(n), 8);
 
-    expect(got).toEqual(want)
+    expect(got).toEqual(want);
+  });
+});
+
+describe('readVarInt', () => {
+  test('correct conversion for small value', () => {
+    const bytes = new Uint8Array([0x01]);
+
+    const want = 1;
+    const got = readVarInt(bytes);
+
+    expect(got).toEqual(want);
+  });
+
+  test('correct conversion for medium value', () => {
+    const bytes = new Uint8Array([0xfd, 0xf4, 0x01]); // little endian 0x01f4
+
+    const want = 0x01f4n;
+    const got = readVarInt(bytes);
+
+    expect(got).toEqual(want);
+  });
+
+  test('correct conversion for large value', () => {
+    const bytes = new Uint8Array([0xfe, 0xa0, 0x86, 0x01, 0x00]);
+
+    const want = 0x0186a0n;
+    const got = readVarInt(bytes);
+
+    expect(got).toEqual(want);
+  });
+
+  test('correct conversion for very large value', () => {
+    const bytes = new Uint8Array([0xff, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+    // const want = 0x010000000000000000n;
+    const want = 1n;
+    const got = readVarInt(bytes);
+    expect(got).toEqual(want);
   });
 });
