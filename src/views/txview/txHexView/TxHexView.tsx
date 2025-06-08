5149764d6cd570244ca0d095fabbf44f26684cf7
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-06-08 18:30:26 -0300

    hmmm

--- src/views/txview/txHexView/TxHexView.tsx
+++ src/views/txview/txHexView/TxHexView.tsx
@@ -1,12 +1,14 @@
 import Tx from '@/crypto/transaction/Tx';
 import './tx-hex-view.css';
 
-
 type TXHVProps = {
   tx: Tx;
 };
 
 export default function TxHexView({ tx }: TXHVProps) {
-
-  return <div className="flex-column tx-hex-view-container">TxHexView</div>;
+  return (
+    <div className="flex-column tx-hex-view-container">
+      <p> {tx.toHex()}</p>
+    </div>
+  );
 }
