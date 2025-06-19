f0a3faaeb4201d77762b42d961cf787b72780398
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-06-19 12:11:48 -0300

    icons for debug controls...

--- src/views/scriptView/ScriptView.tsx
+++ src/views/scriptView/ScriptView.tsx
@@ -1,9 +1,13 @@
 import { ScriptEditor } from './scriptEditor/ScriptEditor';
 
 import './script-view.css';
-import { useDebugStore } from '@/state/debugStore';
 import { Stack } from './stack/Stack';
-import { useScriptDebugger } from '@hooks/useScriptDebugger';
+
+import Stepover from '@assets/icons/stepover.svg?react';
+import Play from '@assets/icons/play.svg?react';
+import Reset from '@assets/icons/reset.svg?react';
+import { useScriptDebugger } from '@/hooks/useScriptDebugger';
+import { useDebugStore } from '@/state/debugStore';
 
 export default function ScriptView() {
   const { reset, setStatus } = useDebugStore();
@@ -11,7 +15,7 @@ export default function ScriptView() {
 
   return (
     <div>
-      <div>
+      {/* <div>
         <div className="flex-column">
           <h1>Debug Controls (WIP)</h1>
         </div>
@@ -33,10 +37,31 @@ export default function ScriptView() {
             Reset
           </button>
         </div>
-      </div>
+      </div> */}
       <div className="flex-row script-view-container">
         <ScriptEditor />
-
+        <div className="flex-column debug-controls-container">
+          <h3>Controls</h3>
+          <div className="flex-row control-dock">
+            <Play onClick={() => {}} height={24} width={24} style={{ fill: 'var(--soft-green)' }} />
+            <Stepover
+              onClick={() => {
+                const status = step();
+                setStatus(status);
+              }}
+              height={20}
+              width={20}
+              style={{ fill: 'var(--sky-blue)' }}
+            />
+            <Reset
+              onClick={() => {
+                reset();
+              }}
+              height={22}
+              width={22}
+            />
+          </div>
+        </div>
         <Stack />
       </div>
     </div>
