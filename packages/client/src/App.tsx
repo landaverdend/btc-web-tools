88dcc7698f1fb28011723b9aa2ab659dea8d9312
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-07-06 16:49:45 -0300

    adding routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,26 +1,25 @@
 import Navbar from '@components/navbar/Navbar';
 import TxView from '@views/txview/TxView';
-import { useState } from 'react';
 import ScriptView from '@views/scriptView/ScriptView';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-export type ViewType = 'tx' | 'script';
+export const APP_ROUTES = {
+  Script: '/script',
+  Tx: '/tx',
+  Home: '/',
+};
 
 export default function App() {
-  const [view, setView] = useState<ViewType>('script');
-
-  const renderView = () => {
-    switch (view) {
-      case 'tx':
-        return <TxView />;
-      case 'script':
-        return <ScriptView />;
-    }
-  };
-
   return (
-    <div>
-      <Navbar view={view} setView={setView} />
-      {renderView()}
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/script" element={<ScriptView />} />
+          <Route path="/tx" element={<TxView />} />
+          <Route path="/" element={<Navigate to="/script" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
--- packages/client/src/components/navbar/Navbar.tsx
+++ packages/client/src/components/navbar/Navbar.tsx
@@ -1,30 +1,28 @@
-import { ViewType } from '@/App';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 import logo from '@assets/btc.png';
+import { Flex } from 'antd';
+import { APP_ROUTES } from '@/App';
 
-type NavbarProps = {
-  view: ViewType;
-  setView: (view: ViewType) => void;
-};
-export default function Navbar({ view, setView }: NavbarProps) {
+type NavbarProps = {};
+export default function Navbar({}: NavbarProps) {
+  const location = useLocation();
+  const currentView = location.pathname.substring(1) || 'script';
+
   return (
-    <div className="navbar-container">
+    <Flex align="center" justify="space-between" className="navbar-container">
       <div className="navbar-logo">
         <img src={logo} alt="logo" />
       </div>
 
-      <div className="flex-row navbar-list">
-        <span
-          className={`navbar-list-item ${view === 'tx' ? 'active' : ''}`}
-          onClick={() => {
-            setView('tx');
-          }}>
-          TX Viewer 
-        </span>
-        <span className={`navbar-list-item ${view === 'script' ? 'active' : ''}`} onClick={() => setView('script')}>
+      <Flex className="navbar-list" gap={10}>
+        <Link to={APP_ROUTES.Tx} className={`navbar-list-item ${currentView === 'tx' ? 'active' : ''}`}>
+          TX Viewer
+        </Link>
+        <Link to={APP_ROUTES.Script} className={`navbar-list-item ${currentView === 'script' ? 'active' : ''}`}>
           Script Debugger
-        </span>
-      </div>
-    </div>
+        </Link>
+      </Flex>
+    </Flex>
   );
 }
