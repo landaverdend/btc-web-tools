7e55e7cc0c059bf659ee37592aa4fa1298d2102c
Nicodemus  Landaverde <nicodemus.landaverde98@gmail.com>
2025-07-10 23:40:48 -0300

    looking slicker

--- packages/client/src/views/merkleView/MerkleView.tsx
+++ packages/client/src/views/merkleView/MerkleView.tsx
@@ -1,9 +1,10 @@
-import { Input } from 'antd';
+import { Button, Flex, Input } from 'antd';
 import './merkle-view.css';
 import Tree, { CustomNodeElementProps, RawNodeDatum } from 'react-d3-tree';
 import { hash256 } from '@/btclib/hash/hashUtil';
 import { bytesToHex } from '@/btclib/util/helper';
 import { useState } from 'react';
+import ColoredText from '@/components/coloredText/ColoredText';
 
 // Build a merkle tree from a list of input strings
 function generateTree(inputs: string[]): RawNodeDatum {
@@ -38,8 +39,8 @@ const InputNode = (rd3tProps: CustomNodeElementProps, onLeafChange: (str: string
 
   const isLeaf = nodeDatum.children === undefined || nodeDatum.children.length === 0;
 
-  // foreign obj dims
-  const width = 100;
+  // Input Leaf dims
+  const width = 115;
   const height = 30;
   const x = -width / 2;
   const y = -height / 2;
@@ -61,6 +62,7 @@ const InputNode = (rd3tProps: CustomNodeElementProps, onLeafChange: (str: string
               width: '100%',
               height: '100%',
               zIndex: 1000,
+              border: '2px solid var(--sky-blue)',
             }}
             value={nodeDatum.name}
             onChange={(e) => {
@@ -72,9 +74,18 @@ const InputNode = (rd3tProps: CustomNodeElementProps, onLeafChange: (str: string
           />
         </foreignObject>
       )}
+
       {!isLeaf && (
         <>
-          <rect x={hashX} y={rectY} width={hashWidth * 1.1} height={rectHeight} fill="var(--soft-orange)" />
+          <rect
+            x={hashX}
+            y={rectY}
+            width={hashWidth * 1.1}
+            height={rectHeight}
+            fill="var(--input-gray)"
+            stroke="white"
+            strokeWidth={2}
+          />
           <foreignObject width={hashWidth} height={height} x={hashX * 0.95} y={y}>
             <span className="hash-text">{nodeDatum.name}</span>
           </foreignObject>
@@ -84,22 +95,19 @@ const InputNode = (rd3tProps: CustomNodeElementProps, onLeafChange: (str: string
   );
 };
 
-const initialInputs = () => {
+const initialInputs = (numInputs: number) => {
   // build the initial tree
   const inputs = [];
 
-  const a_char_index = 97;
-  const endIndex = a_char_index + 6;
-
-  for (let i = a_char_index; i < endIndex; i++) {
-    inputs.push(String.fromCharCode(i));
+  for (let i = 0; i < numInputs; i++) {
+    inputs.push(`TX Data ${i + 1}`);
   }
 
   return inputs;
 };
 
 export default function MerkleView() {
-  const [inputs, setInputs] = useState<string[]>(initialInputs());
+  const [inputs, setInputs] = useState<string[]>(initialInputs(5));
 
   const tree = generateTree(inputs);
 
@@ -111,17 +119,35 @@ export default function MerkleView() {
 
   // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <span>Merkle Root: {tree.name}</span>
+    <Flex className="merkle-view-container" style={{ width: '100vw', height: '100vh' }} vertical justify="center" align="center">
+      <Flex className="merkle-view-header" vertical justify="center" align="center" gap={10}>
+        <span className="merkle-root-text">
+          Merkle Root: <ColoredText color="var(--sky-blue)">{tree.name}</ColoredText>
+        </span>
+
+        <p>
+          A merkle tree is a binary tree where each node is a hash of its children. In Bitcoin, merkle trees are used to create a
+          compact and easily verifiable fingerprint of a block and its transactions.
+        </p>
+
+        <Flex gap={5}>
+          <Button onClick={() => setInputs([...inputs, `TX Data ${inputs.length + 1}`])}>Add Leaf Node</Button>
+          <Button onClick={() => setInputs(inputs.slice(0, -1))}>Remove Leaf Node</Button>
+        </Flex>
+      </Flex>
+
       <Tree
         data={tree}
         orientation="vertical"
         depthFactor={-100}
+        pathClassFunc={(path) => {
+          return 'merkle-tree-path';
+        }}
         translate={{ x: window.innerWidth * 0.5, y: window.innerHeight * 0.5 }}
         renderCustomNodeElement={(rd3tProps) => {
           return InputNode(rd3tProps, onLeafChange);
         }}
       />
-    </div>
+    </Flex>
   );
 }
